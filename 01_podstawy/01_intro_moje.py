6fad9bb0a59bd0e8fd6ae47566d79febf1385c4e
Przemysław Górecki <minutnik75@gmail.com>
2022-05-01 16:41:00 +0200

    Inicjalny commit

---
+++ 01_podstawy/01_intro_moje.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# tu będzie komentarz
+
+# %%
+print(2 + 2)
+
+# %%
+print (3 * 3)
+
+# %%
+print(3 + 2 * 2)
+print(4 - 2 * 2)
+
+# %%
+print(10 / 3)
+
+print (4 / 2)
+
+# %%
+# dzielenie - czesc całkowiata
+print (7 // 5)
+
+# %%
+# dzielenie - reszta
+print (7 % 5)
+# %%
+# potegowanie
+print (2 ** 5)
+
+
+
+
+# %%
+# - przypisanie zmiennym wartosci
+
+a = 10
+b = 20
+
+c = a * b
+print (c)
+
+# %%
+print('love python')
+print("It's the best")
+print('It\s the best')
+
+# %%
+print ("Python \n 3.7")
+
+print (""" Python
+3.7 """)
